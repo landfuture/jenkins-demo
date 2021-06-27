b6ca88bad39b50c448ac076bd01bb1eb5e50a052
root <root@localhost.localdomain>
2021-06-26 20:18:56 -0400

    Add existing file-cjs

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/landfuture/spring-boot-api-example'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
